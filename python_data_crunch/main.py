dbb17e1e7f4ab8da5bc354571c5914d10bc4f6f6
wolfwlj <86714@glr.nl>
2023-11-14 01:06:14 +0100

    2222ffffffffffff

--- python_data_crunch/main.py
+++ python_data_crunch/main.py
@@ -279,5 +279,5 @@ def read_item(gender: str, height: int, weight: int, age: int, goal: str, activi
 if __name__ == "__main__":
     import uvicorn
     import os   
-    print(os.getenv('$PORT'))
-    uvicorn.run(app, host="0.0.0.0", port=os.getenv('$PORT'))
+    print(os.getenv('PORT'))
+    uvicorn.run(app, host="0.0.0.0", port=os.getenv('PORT'))
